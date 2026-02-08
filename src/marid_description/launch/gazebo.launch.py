74ffdc9208690e1a5d3b1e23cc78d7b38e72379c
MChalak1 <mc@todo.todo>
2026-02-08 15:56:07 -0800

    Add camera sensor for perception and ML

--- src/marid_description/launch/gazebo.launch.py
+++ src/marid_description/launch/gazebo.launch.py
@@ -96,7 +96,10 @@ def generate_launch_description():
         "/model/marid/joint/tail_left_joint/cmd_pos@std_msgs/msg/Float64]gz.msgs.Double",
         "/model/marid/joint/tail_right_joint/cmd_pos@std_msgs/msg/Float64]gz.msgs.Double",
         "/lidar/scan@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked",
-        # Note: /airspeed is gz.msgs.AirSpeed, not Double. Use airspeed_converter.py node instead.
+        # Camera bridge:
+        "/camera/image_raw@sensor_msgs/msg/Image[gz.msgs.Image",
+        "/camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo",
+    
     ],
     output='screen'
     )
